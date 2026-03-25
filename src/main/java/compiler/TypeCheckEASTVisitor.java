ed4494c8f701b15f76a60ea63ad11f865cd8acb1
Alessandro Ronchi <alessandro.ronchi0018@gmail.com>
2026-03-25 21:07:29 +0100

    Finished and tested implementation of Minus

--- src/main/java/compiler/TypeCheckEASTVisitor.java
+++ src/main/java/compiler/TypeCheckEASTVisitor.java
@@ -169,6 +169,14 @@ public class TypeCheckEASTVisitor extends BaseEASTVisitor<TypeNode, TypeExceptio
     }
 
     @Override
+    public TypeNode visitNode(MinusNode n) throws TypeException {
+        if (print) printNode(n);
+        if (!(isSubtype(visit(n.left), new IntTypeNode()) && isSubtype(visit(n.right), new IntTypeNode())))
+            throw new TypeException("Non integers in subtraction", n.getLine());
+        return new IntTypeNode();
+    }
+
+    @Override
     public TypeNode visitNode(CallNode n) throws TypeException {
         if (print) printNode(n, n.id);
         TypeNode t = visit(n.entry);
