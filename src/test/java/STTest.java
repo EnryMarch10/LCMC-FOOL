56170a93e33aa3ca7b69cb83dff82d1a160c93ae
Alessandro Ronchi <alessandro.ronchi0018@gmail.com>
2026-04-23 20:31:35 +0200

    Added tests for Object Oriented extension of SymbolTableASTVisitor

--- src/test/java/STTest.java
+++ src/test/java/STTest.java
@@ -7,36 +7,75 @@ public class STTest {
 
     @Test
     void testMultiplyDeclaredFun() {
-        assertEquals(1,
-            ProgramsTest.getSymbolTableErrors(ProgramsTest.fromFile(ST + "prova1_multiply_declared_fun.fool")));
+        assertEquals(
+            1, ProgramsTest.getSymbolTableErrors(ProgramsTest.fromFile(ST + "prova1_multiply_declared_fun.fool")));
     }
 
     @Test
     void testMultiplyDeclaredPar() {
-        assertEquals(1,
-            ProgramsTest.getSymbolTableErrors(ProgramsTest.fromFile(ST + "prova1_multiply_declared_par.fool")));
+        assertEquals(
+            1, ProgramsTest.getSymbolTableErrors(ProgramsTest.fromFile(ST + "prova1_multiply_declared_par.fool")));
     }
 
     @Test
     void testMultiplyDeclaredVar() {
-        assertEquals(1,
-            ProgramsTest.getSymbolTableErrors(ProgramsTest.fromFile(ST + "prova1_multiply_declared_var.fool")));
+        assertEquals(
+            1, ProgramsTest.getSymbolTableErrors(ProgramsTest.fromFile(ST + "prova1_multiply_declared_var.fool")));
     }
 
     @Test
     void testUndeclaredFun() {
-        assertEquals(1,
-            ProgramsTest.getSymbolTableErrors(ProgramsTest.fromFile(ST + "prova1_undeclared_fun.fool")));
+        assertEquals(1, ProgramsTest.getSymbolTableErrors(ProgramsTest.fromFile(ST + "prova1_undeclared_fun.fool")));
     }
 
     @Test
     void testUndeclaredVar() {
-        assertEquals(2,
-            ProgramsTest.getSymbolTableErrors(ProgramsTest.fromFile(ST + "prova1_undeclared_var.fool")));
+        assertEquals(2, ProgramsTest.getSymbolTableErrors(ProgramsTest.fromFile(ST + "prova1_undeclared_var.fool")));
     }
 
     @Test
     void testMultiplyDeclaredClass() {
-        // TODO: test
+        assertEquals(
+            4,
+            ProgramsTest.getSymbolTableErrors(ProgramsTest.fromFile(ST + "vector_multiply_declared_class.fool")));
+    }
+
+    @Test
+    void testMultiplyDeclaredField() {
+        assertEquals(
+            1,
+            ProgramsTest.getSymbolTableErrors(ProgramsTest.fromFile(ST + "vector_multiply_declared_field.fool")));
+    }
+
+    @Test
+    void testMultiplyDeclaredMethod() {
+        assertEquals(
+            1,
+            ProgramsTest.getSymbolTableErrors(ProgramsTest.fromFile(ST + "vector_multiply_declared_method.fool")));
+    }
+
+    @Test
+    void testMultiplyDeclaredMethodParAndReferenceIdIsNotAReferenceType() {
+        assertEquals(
+            3,
+            ProgramsTest.getSymbolTableErrors(
+                ProgramsTest.fromFile(ST + "vector_multiply_declared_method_par.fool")));
+    }
+
+    @Test
+    void testUndeclaredReferenceId() {
+        assertEquals(
+            1,
+            ProgramsTest.getSymbolTableErrors(ProgramsTest.fromFile(ST + "vector_undeclared_reference_id.fool")));
+    }
+
+    @Test
+    void testUndeclaredMethod() {
+        assertEquals(2, ProgramsTest.getSymbolTableErrors(ProgramsTest.fromFile(ST + "vector_undeclared_method.fool")));
+    }
+
+    @Test
+    void testUndeclaredClass() {
+        assertEquals(5, ProgramsTest.getSymbolTableErrors(ProgramsTest.fromFile(ST + "vector_undeclared_class.fool")));
     }
 }
