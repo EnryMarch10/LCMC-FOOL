4c9c5185d91dc3edb316865dd76d7a16c050b435
Alessandro Ronchi <alessandro.ronchi0018@gmail.com>
2026-05-07 11:27:36 +0200

    Wrote automatic tests for code generation

---
+++ src/test/java/CodeGenerationTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class CodeGenerationTest {
+    @ParameterizedTest
+    @CsvSource({
+        "integer, 4",
+        "quicksort, 122345",
+        "vector, 1426",
+        "vector_equals_test, 101"
+        // "bankloan, 50_000" TODO: uncomment when implementing hereditariness
+    })
+    void testCorrectOutput(String fileName, String expectedResult) {
+        ProgramsTest.testResultFromFile(ProgramsTest.SAMPLES + fileName + ".fool", expectedResult);
+    }
+}
